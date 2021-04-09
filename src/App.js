719c2fe05197941c220924ef1f3f038b9733d0f2
Yvonne Lange <yvonne-lange@web.de>
2021-04-09 12:02:05 +0200

    Add components folders

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App">Harry Potter</div>;
+  return (
+    <Router>
+      <div className="App">Harry Potter</div>
+      <header></header>
+      <main></main>
+      <footer></footer>
+    </Router>
+  );
 }
 
 export default App;
