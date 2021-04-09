7ad78fa9b6476181fa0ed066917d865f86788421
Yvonne Lange <yvonne-lange@web.de>
2021-04-09 14:39:48 +0200

    Add base structure of the header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Characters from "./components/Characters";
 
 function App() {
   return (
     <Router>
-      <div className="App">Harry Potter</div>
-      <header></header>
-      <main></main>
-      <footer></footer>
+      <div className="App">
+        <header className="header">
+          <Header />
+        </header>
+        <main className="main">
+          <Switch>
+            <Route path="/characters">
+              <Characters />
+            </Route>
+          </Switch>
+        </main>
+        <footer className="footer"></footer>
+      </div>
     </Router>
   );
 }
